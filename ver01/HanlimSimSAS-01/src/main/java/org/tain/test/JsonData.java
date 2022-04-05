6b44b4bb3f74835fbc0707ba2301afaa976eed6e
gksruf2848 <4455qqq@naver.com>
2022-04-05 20:39:58 +0900

    commit

---
+++ ver01/HanlimSimSAS-01/src/main/java/org/tain/test/JsonData.java
@@ -0,0 +1,34 @@
+package org.tain.test;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonData {
+	
+private static final boolean flag = true;
+	
+	public static void main(String[] args) throws Exception {
+		if (flag) test01();
+	}
+	
+	private static void test01() throws Exception {
+		String jsonString = getJsonString();
+		System.out.println(jsonString);
+	}
+	
+	private static String getJsonString() {
+		StringBuilder sb = new StringBuilder();
+		String comma = "";
+		sb.append("[");
+		for (int i=100; i < 110; i++) {
+			sb.append(comma);
+			sb.append(String.format("{\"id\":%d, \"campCode\":\"code%d\", \"campContent\":\"content%d\"}", i, i, i));
+			comma = ",";
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+}
