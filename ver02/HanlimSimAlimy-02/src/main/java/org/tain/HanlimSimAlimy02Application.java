fcd6cafaf54f0c5469d8065d678a161d73e02ddd
gksruf2848 <4455qqq@naver.com>
2022-04-06 13:50:26 +0900

    ver02 Create

---
+++ ver02/HanlimSimAlimy-02/src/main/java/org/tain/HanlimSimAlimy02Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanlimSimAlimy02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanlimSimAlimy02Application.class, args);
+	}
+
+}
---
+++ ver02/HanlimSimSAS-02/src/main/java/org/tain/HanlimSimSas02Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanlimSimSas02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanlimSimSas02Application.class, args);
+	}
+
+}
